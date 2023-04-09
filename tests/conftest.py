4a5a67115d59545551b78f947b6905e3bce9a495
aselka <bis-aselya@yandex.ru>
2023-04-10 01:01:31 +0400

    commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,32 @@
+import allure
+import allure_commons
 import pytest
-from appium import webdriver
-from appium.options.android import UiAutomator2Options
-from pydantic import BaseModel
-import os
-from dotenv import load_dotenv
 from selene.support.shared import browser
-from utils.attach import add_video
-from utils.attach import add_screenshot
+from selene import support
+from appium import webdriver
+import config
+from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
-def data_search():
-    load_dotenv()
+def app():
+    browser.config.timeout = config.settings.timeout
 
-    class Settings(BaseModel):
-        load_dotenv()
-        userName: str = os.getenv('LOGIN')
-        accessKey: str = os.getenv('KEY')
-        remote_url: str = os.getenv('REMOTE')
-
-    settings = Settings()
-    options = UiAutomator2Options().load_capabilities({
-        "app": "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c",
-        "platformVersion": "9.0",
-        "deviceName": "Google Pixel 3",
-        'bstack:options': {
-            "projectName": "Second Python project",
-            "buildName": "browserstack-build-2",
-            "sessionName": "BStack second_test",
-            'userName': settings.userName,
-            'accessKey': settings.accessKey
-        }
-    })
-
-    browser.config.driver = webdriver.Remote(settings.remote_url, options=options)
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext
+    )
+    browser.config.driver = webdriver.Remote(
+        config.settings.remote_url, options=config.settings.driver_options
+    )
 
     yield
 
-    add_video(browser)
-    add_screenshot(browser)
+    attach.add_video(browser)
+    attach.add_screenshot(browser)
+
     browser.quit()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def add_labels():
+    allure.dynamic.label('owner', 'asel')
