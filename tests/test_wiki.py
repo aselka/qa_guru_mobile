1ed71dffde9aace6fcf2944826d548f2ec5cc8f4
aselka <bis-aselya@yandex.ru>
2023-04-10 01:44:28 +0400

    commit change

--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -7,8 +7,8 @@ from appium.webdriver.common.appiumby import AppiumBy
 
 
 def test_search_should_find_results():
-    browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
-    browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).send_keys('BrowserStack')
+    browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search QA GURU')).click()
+    browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).send_keys('QA GURU')
     browser.all((AppiumBy.CLASS_NAME, 'android.widget.TextView')).should(have.size_greater_than(0))
 
 
