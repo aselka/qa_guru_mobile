f9cdf8789be43983750fbf86bc541677ff2005aa
aselka <bis-aselya@yandex.ru>
2023-04-18 22:59:31 +0400

    commit with new tests

--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -2,13 +2,13 @@ import pytest
 import allure
 from selene import have
 from selene.support.shared import browser
-from appium.webdriver.common.appiumby import AppiumBy
+from utils.app_locators import *
 
 
 @pytest.fixture()
 def skip():
     with allure.step('Пропуск стартовых экранов'):
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+        browser.element(btn_skip).click()
 
 
 @allure.label('owner', 'bisengalieva')
@@ -16,20 +16,22 @@ def skip():
 @allure.title('Результат поиска не пустой')
 def test_search_should_find_results():
     with allure.step('Переход на экран поиска и ввод искомого значения'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).send_keys('QA GURU')
+        browser.element(search).click()
+        browser.element(field_search).send_keys('QA GURU')
     with allure.step('Проверка результатов поиска'):
-        browser.all((AppiumBy.CLASS_NAME, 'android.widget.TextView')).should(have.size_greater_than(0))
+        browser.all(result_search).should(have.size_greater_than(0))
 
 
 @allure.label('owner', 'bisengalieva')
-@allure.title('Test empty search')
+@allure.feature('Тесты Wikipedia')
+@allure.title('Пустой поиск')
 def test_empty_search():
     with allure.step('Поиск пустого значения'):
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).send_keys('')
+        browser.element(search).click()
+        browser.element(field_search).send_keys('')
 
     with allure.step('Проверка результатов поиска'):
-        browser.all((AppiumBy.CLASS_NAME, 'android.widget.TextView')).should(have.size_greater_than(0))
+        browser.all(result_search).should(have.size_greater_than(0))
 
 
 @allure.label('owner', 'bisengalieva')
@@ -37,4 +39,4 @@ def test_empty_search():
 @allure.title('Переход на главную по кнопке "skip"')
 def test_skip_main_screens(skip):
     with allure.step('Проверка отображения главного экрана'):
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/view_announcement_text')).should(have.text('Customize your Explore feed'))
+        browser.element(ann_main_screen).should(have.text('Customize your Explore feed'))
