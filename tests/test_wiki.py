cf145f1684c320ff4d928e44ba19afed79621420
aselka <bis-aselya@yandex.ru>
2023-04-19 00:26:44 +0400

    new commit

--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -17,34 +17,16 @@ def test_search_should_find_results():
 
 @allure.label('owner', 'bisengalieva')
 @allure.feature('Тесты Wikipedia')
-@allure.title('Пустой поиск')
-def test_empty_search():
-    with allure.step('Переход на экран поиска и ввод искомого значения'):
-        browser.element(search).click()
-        browser.element(field_search).send_keys('')
-
-    with allure.step('Проверка результатов поиска'):
-        browser.all(result_search).should(have.size(0))
-
-
-@allure.label('owner', 'bisengalieva')
-@allure.feature('Тесты Wikipedia')
-@allure.title('Поиск цифр')
-def test_search_should_find_results():
-    with allure.step('Переход на экран поиска и ввод искомого значения'):
-        browser.element(search).click()
-        browser.element(field_search).send_keys('34567')
-    with allure.step('Проверка результатов поиска'):
-        browser.all(result_search).should(have.size_greater_than(0))
-
-
-@allure.label('owner', 'bisengalieva')
-@allure.feature('Тесты Wikipedia')
-@allure.title('Поиск символов')
-def test_search_should_find_results():
-    with allure.step('Переход на экран поиска и ввод искомого значения'):
-        browser.element(search).click()
-        browser.element(field_search).send_keys('?%-')
-    with allure.step('Проверка результатов поиска'):
-        browser.all(result_search).should(have.size_greater_than(0))
-
+@allure.title('Навигация по экранам приложения Wikipedia')
+def test_skip_screens():
+    with allure.step('Проверка стартового экрана'):
+        browser.element(title).should(have.text("The Free Encyclopedia"))
+        browser.element(btn_continue).click()
+    with allure.step('Проверка перехода к второму экрана'):
+        browser.element(title).should(have.text("New ways to explore"))
+        browser.element(btn_continue).click()
+    with allure.step('Проверка перехода к третьему экрану'):
+        browser.element(title).should(have.exact_text("Reading lists with sync"))
+        browser.element(btn_continue).click()
+    with allure.step('Проверка перехода к четвертому экрана'):
+        browser.element(title).should(have.text("Send anonymous data"))
